eaaadf4d68b68c71bc3a787b7a88cf54db2c4e45
butschster <butschster@gmail.com>
2016-02-12 17:37:03 +0300

    init

---
+++ config/permissions.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'email' => [
+        'event.list',
+        'event.create',
+        'event.edit',
+        'event.delete',
+        'template.list',
+        'template.create',
+        'template.edit',
+        'template.delete',
+    ],
+];
